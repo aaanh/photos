04ebc6a51f15ad1deb1e254c561827597f982978
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-09-30 18:30:20 -0400

    rewrite history

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import DefaultWrapperLayout from '../layouts/DefaultWrapperLayout/DefaultWrapperLayout'
+
+const ContactPage = () => {
+  return (
+    <DefaultWrapperLayout title="Contact" description="Hit me up!">
+      <div></div>
+    </DefaultWrapperLayout>
+  )
+}
+
+export default ContactPage
