ea3eb114d4a86f657eeba5c9e75f319cc03a613a
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-10-02 00:51:45 -0400

    fixed about, added contacts

--- pages/about.js
+++ pages/about.js
@@ -6,14 +6,14 @@ const AboutPage = () => {
   return (
     <DefaultWrapperLayout>
       <SEO title="About | Anh's Photography" description="What's going on?"></SEO>
-      <div className='p-4'>
+      <div className='p-4 flex w-screen justify-center flex-col items-center'>
         <h1 className='text-4xl'>About This Site</h1>
         <div className='flex flex-col my-2 space-y-2'>
           <h2 className='text-2xl'>Technical Information</h2>
           <ul>
-            <li>Built with RedwoodJS</li>
+            <li>Built with NextJS</li>
             <li>Asset storage with Cloudinary</li>
-            <li>Database and API with Prisma</li>
+            <li>Custom Database and API query</li>
             <li>Hosted on Vercel</li>
             <li>DNS and CDN with Cloudflare</li>
             <li>Source code: <a className='underline underline-offset-2 hover:font-semibold' href="https://github.com/aaanh/my-photo-reel">Github</a></li>
--- pages/contact.js
+++ pages/contact.js
@@ -4,8 +4,15 @@ import SEO from '../components/SEO'
 const ContactPage = () => {
   return (
     <DefaultWrapperLayout>
-      <SEO title="Contact | Anh's Photography" description="Hit me up!"></SEO>
-      <div></div>
+      <SEO title="Contact | Anh's Photography" description="hmu fam"></SEO>
+      <div className='p-4 flex w-screen justify-center flex-col items-center'>
+        <h1 className='text-4xl'>Contacts</h1>
+        <div className='flex flex-col my-2 space-y-2'>
+          <ul>
+            <li>All business and general inquiries: <a className='underline underline-offset-2 hover:font-semibold' href="mailto:overlord@hoanganh.dev">{'overlord [at] hoanganh [dot] dev'}</a></li>
+          </ul>
+        </div>
+      </div>
     </DefaultWrapperLayout>
   )
 }
