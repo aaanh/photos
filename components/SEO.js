ae2fc2e3b8f241238899ffb5da279aed33bd0c80
Anh H Nguyen <iam@hoanganh.dev>
2023-07-17 16:26:33 -0400

    Update SEO

--- components/SEO.js
+++ components/SEO.js
@@ -8,7 +8,7 @@ const SEO = ({ title, description }) => {
       <link rel="icon" href="/logo-color-variant.png"></link>
       <meta
         property="og:image"
-        content="https://res.cloudinary.com/aaanh/image/upload/w_500,c_scale/v1689623648/2023/DSC_5005_nozego.jpgg"
+        content="https://res.cloudinary.com/aaanh/image/upload/w_500,c_scale/v1689623648/2023/DSC_5005_nozego.jpg"
       />
     </Head>
   );
