7e40533504f08d95bd5486710b3e8176f1d43330
Anh H Nguyen <iam@hoanganh.dev>
2025-03-10 13:31:46 -0400

    Add t3 env (#8)
    * Add t3 env
    
    * add jiti
    
    * Remove jiti

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "res.cloudinary.com"
-      }
-    ]
-  }
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
