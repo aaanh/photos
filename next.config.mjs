6549b115b2ec9710f2f15b0e6fc43dfec6915486
Anh H Nguyen <AnhNguyen@aaanh.com>
2025-03-10 13:21:12 -0400

    Remove jiti

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
-import { fileURLToPath } from "node:url";
-import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-jiti("./app/env");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
